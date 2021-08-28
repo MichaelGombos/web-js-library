50315993d5dbd4bf2faf2cdec1f340f9b4937fb6
Michael Gombos <gombos2002@gmail.com>
2021-08-28 18:45:45 -0500

    feat: added functionality to remove elements

--- main.js
+++ main.js
@@ -3,8 +3,26 @@
 //This object is placed into an array of books
 //Once an object is placed or removed.. Display the library
 
-let library = new Array();
-let domLibrary = document.getElementById("library-array");
+//Getting dom elements
+const domLibrary = document.getElementById("library-array");
+//Library form
+const libraryForm = document.getElementById("library-form");
+const bookNameInput = document.getElementById("book");
+const bookAuthorInput = document.getElementById("author");
+const bookPagesInput = document.getElementById("pages");
+const bookReadInput = document.getElementById("read");
+
+const primarySubmitButton = document.getElementById("primary-submit");
+primarySubmitButton.addEventListener("click", (e) => {
+  let domBook = new Book(
+    bookNameInput.value,
+    bookAuthorInput.value,
+    bookPagesInput.value,
+    bookReadInput.value
+  );
+
+  addToLibrary(domBook);
+});
 
 function Book(name, author, pages, read) {
   this.name = name;
@@ -34,16 +52,18 @@ function addToLibrary(book) {
   <div class="col-1 my-auto">
     <p class="my-auto book-read">${book.read}</p>
   </div>
-  <div class="col-2 offset-1 btn btn-outline-danger">Remove</div>
+  <div class="col-2 offset-1 btn btn-outline-danger remove-button">Remove</div>
 </div>
   `;
+
+  const removeButton = bookElement.getElementsByClassName("remove-button");
+  console.log(Array.from(removeButton));
+
+  removeButton[0].addEventListener("click", (e) =>
+    domLibrary.removeChild(bookElement)
+  );
+
   domLibrary.appendChild(bookElement);
 }
 
-function removeFromLibrary(book) {
-  //TODO
-}
-
-let naruto = new Book("CringeRuto", "michael gombos", "100", "false");
-addToLibrary(naruto);
-addToLibrary(naruto);
+function removeFromLibrary() {}
